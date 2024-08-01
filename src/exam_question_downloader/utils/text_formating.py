dbb641133d6190b833bf2aaa8592cd2d88b71a15
Bea <bea@universitatpolitecnicaval.es>
2024-08-01 19:29:19 +0200

    Nuevo notebook para examinar todas las preguntas de los CSV de un tema concreto; ShowFromCSV

--- src/exam_question_downloader/utils/text_formating.py
+++ src/exam_question_downloader/utils/text_formating.py
@@ -1,8 +1,14 @@
 import textwrap
+import pandas
 
 def format_question(q):
     ANCHO=120
-    result=f"{textwrap.fill(q.question, width=ANCHO)}\n  a) {textwrap.fill(q.a, width=ANCHO)}\n  b) {textwrap.fill(q.b, width=ANCHO)}\n  c) {textwrap.fill(q.c, width=ANCHO)}\n  d) {textwrap.fill(q.d, width=ANCHO)}"
+    question='' if pandas.isna(q.question) else textwrap.fill(q.question, width=ANCHO)
+    opcion_a='' if pandas.isna(q.a) else textwrap.fill(q.a, width=ANCHO)
+    opcion_b='' if pandas.isna(q.b) else textwrap.fill(q.b, width=ANCHO)
+    opcion_c='' if pandas.isna(q.c) else textwrap.fill(q.c, width=ANCHO)
+    opcion_d='' if pandas.isna(q.d) else textwrap.fill(q.d, width=ANCHO)
+    result=f"{question}\n  a) {opcion_a}\n  b) {opcion_b}\n  c) {opcion_c}\n  d) {opcion_d}"
     return result
 
 def format_questions(questions):
